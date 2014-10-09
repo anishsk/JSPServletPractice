90dd4fb5cc19bc582f8cfd2d58910cdb653a4b82
Anish <anishsk@users.noreply.github.com>
2014-10-09 22:09:20 +0530

    Initial Commit

---
+++ src/Successful.java
@@ -0,0 +1,48 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Successful
+ */
+@WebServlet("/Successful")
+public class Successful extends HttpServlet implements Servlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Successful() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		/*PrintWriter out = response.getWriter();
+		response.setContentType("text/html");
+		out.println("Welcom " + request.getAttribute("uname"));*/
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+		response.setContentType("text/html");
+		out.println("Welcom " + request.getAttribute("uname"));
+	}
+
+}
